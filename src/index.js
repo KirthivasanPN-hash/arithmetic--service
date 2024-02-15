94ee1c2212b83fe249b9c4b8ef26c9ccc0ab2544
KirthivasanPN-hash <72664884+KirthivasanPN-hash@users.noreply.github.com>
2024-02-15 15:50:17 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3001;
-
+const { add } = require("./arithmetica");
 app.use(cors());
 
 app.get("/", (req, res) => {
